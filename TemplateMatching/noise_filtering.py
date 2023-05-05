454f920fc42d5f79d7846b5789cf4fe2111c751c
Marcus Lai <marcus41881653@gmail.com>
2023-05-05 23:11:24 +0800

    gradient analysis for outliers

--- TemplateMatching/noise_filtering.py
+++ TemplateMatching/noise_filtering.py
@@ -5,11 +5,36 @@ import pandas as pd
 
 
 def continuity_filter(data):
+    THRESHOLD = 5
+    WIDTH_SIZE = 10
+    HEIGHT_SIZE = 5
     for csv in data:
         df = pd.read_csv(csv)
+        df['x'] = df['x']+df['w']/2
+        df['y'] = df['y']+df['h']/2
+
         x = df['x']
         y = df['y']
-        plt.plot(x,y,'bo')
+        gradient = np.gradient(y,x)
+        df['gradient'] = gradient
+
+        plt.style.use('bmh')
+        fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2, 2)
+        fig.set_figwidth(WIDTH_SIZE)
+        fig.set_figheight(HEIGHT_SIZE)
+        fig.suptitle('Teeth and Gradient')
+        ax1.plot(x,y,'.-b')
+        ax2.plot(x,gradient,'.-y')
+
+        df.drop(df[df['gradient'] > THRESHOLD].index, inplace = True)
+        df.drop(df[df['gradient'] < -THRESHOLD].index, inplace = True)
+        df = df[df['gradient'].notna()]
+        x = df['x']
+        y = df['y']
+        gradient = df['gradient']
+        print(gradient)
+        ax3.plot(x,y,'.-b')
+        ax4.plot(x,gradient,'.-y')
         plt.show()
 
 
